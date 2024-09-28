4db5cd1ed1a6eb40a4be2eb039cbbcb3536a9dd6
Fes <example.email@example>
2024-09-28 11:43:35 +0300

    added short problem description

--- sorting/349.py
+++ sorting/349.py
@@ -1,8 +1,8 @@
-https://leetcode.com/problems/intersection-of-two-arrays/description/
-Intersection of Two Arrays
-Given two integer arrays nums1 and nums2, return an array of their 
-intersection
-. Each element in the result must be unique and you may return the result in any order.
+# https://leetcode.com/problems/intersection-of-two-arrays/description/
+# Given two integer arrays nums1 and nums2, return an array of their intersection.
+# Each element in the result must be unique, and you may return the result in any order.
+from typing import List
+
 
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
@@ -12,7 +12,8 @@ class Solution:
                 final_list.append(elem)
         return final_list
 
-Using set:
-class Solution:
+
+# Using set:
+class Solution1:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
         return list(set(nums1) & set(nums2))
