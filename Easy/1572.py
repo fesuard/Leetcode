3860da9004f118975b17c128f308afcf9eb459d1
MIRCEA-EDUARD RADU <166901200+fesuard@users.noreply.github.com>
2024-09-29 18:25:10 +0300

    Update 1572.py

--- Easy/1572.py
+++ Easy/1572.py
@@ -1,7 +1,8 @@
 # https://leetcode.com/problems/matrix-diagonal-sum/
 # Matrix Diagonal Sum
-# Given a square matrix mat, return the sum of the matrix diagonals.
-# Only include the sum of all the elements on the primary diagonal and all the elements on the secondary diagonal that are not part of the primary diagonal.
+# Given a square matrix mat, return the sum of the matrix diagonals. Only include 
+# the sum of all the elements on the primary diagonal and all the elements on the 
+# secondary diagonal that are not part of the primary diagonal.
 from typing import List
 
 
