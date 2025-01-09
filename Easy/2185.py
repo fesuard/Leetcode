cca2e5c27514bdaccb037534ef5d65f0f1b4f03f
MIRCEA-EDUARD RADU <166901200+fesuard@users.noreply.github.com>
2025-01-09 18:42:59 +0200

    Update 2185.py

--- Easy/2185.py
+++ Easy/2185.py
@@ -2,13 +2,15 @@
 # You are given an array of strings words and a string pref.
 # Return the number of strings in words that contain pref as a prefix.
 # A prefix of a string s is any leading contiguous substring of s.
-from typing import List
 
 
 class Solution:
     def prefixCount(self, words: List[str], pref: str) -> int:
-        res = []
+        res = 0
+        size = len(pref)
+
         for word in words:
-            if len(word) >= len(pref) and all(word[i] == pref[i] for i in range(len(pref))):
-                res.append(word)
-        return len(res)
+            if word[:size] == pref:
+                res += 1
+
+        return res
