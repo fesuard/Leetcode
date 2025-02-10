1d8862dc0be541e5a5f5b61d42c1dcab9328f5db
MIRCEA-EDUARD RADU <166901200+fesuard@users.noreply.github.com>
2025-02-10 21:10:58 +0200

    Create 1021.py

---
+++ Easy/1021.py
@@ -0,0 +1,28 @@
+# https://leetcode.com/problems/remove-outermost-parentheses/description
+# A valid parentheses string is either empty "", "(" + A + ")", or A + B, where A and B are valid parentheses
+# strings, and + represents string concatenation. For example, "", "()", "(())()", and "(()(()))" are all valid 
+# parentheses strings. A valid parentheses string s is primitive if it is nonempty, and there does not exist a
+# way to split it into s = A + B, with A and B nonempty valid parentheses strings. Given a valid parentheses 
+# string s, consider its primitive decomposition: s = P1 + P2 + ... + Pk, where Pi are primitive valid parentheses
+# strings. Return s after removing the outermost parentheses of every primitive string in the primitive 
+# decomposition of s.
+
+
+class Solution:
+    def removeOuterParentheses(self, s: str) -> str:
+        balance = 0
+        res = []
+
+        for elem in s:
+            if elem == '(':
+                if balance > 0:
+                    res.append(elem)
+                balance += 1
+
+            else:
+                balance -= 1
+                if balance > 0:
+                    res.append(elem)
+
+        return "".join(res)
+            
