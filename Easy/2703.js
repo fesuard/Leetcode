5ca966c13055075c85c14b24243a9ae1c12698ff
MIRCEA-EDUARD RADU <166901200+fesuard@users.noreply.github.com>
2025-04-24 16:46:38 +0300

    Create 2703.js

---
+++ Easy/2703.js
@@ -0,0 +1,7 @@
+// https://leetcode.com/problems/return-length-of-arguments-passed/description
+// Write a function argumentsLength that returns the count of arguments passed to it.
+
+
+const argumentsLength = function(...args) {
+    return args.length
+};
