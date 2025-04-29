a453a6e6ad35ae39f198f6cb8b5a81416058b677
MIRCEA-EDUARD RADU <166901200+fesuard@users.noreply.github.com>
2025-04-29 18:19:57 +0300

    Update 1837.py

--- Easy/1837.py
+++ Easy/1837.py
@@ -4,17 +4,13 @@
 # interpreted as a base 10 number, and the sum should be returned in base 10.
 
 
-# We calculate the number n in base k, store it in num. We iterate num and add its
-# digits to the sum of res.
+# We calculate the number n in base k, and instead of storing the number, we just 
+# add it's digits to res, since it's asking for the sum of the changed number.
 class Solution:
     def sumBase(self, n: int, k: int) -> int:
-        num = ''
-        while n > 0:
-            num += str(n % k)
-            n //= k
-        
         res = 0
-        for digit in num:
-            res += int(digit)
+        while n > 0:
+            res += n % k
+            n //= k
         
         return res
